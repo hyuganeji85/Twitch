34526c60f9916d4bcb4919c561e8974768a0f363
unknown <tomislav.markovski@Tomislav-Markov.seavus.biz>
2012-02-14 14:41:02 +0100

    added blootooth and location services

---
+++ src/mk/t/android/services/BluetoothService.java
@@ -0,0 +1,22 @@
+package mk.t.android.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tomislav.markovski
+ * Date: 2/14/12
+ * Time: 2:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BluetoothService extends IntentService {
+    public BluetoothService() {
+        super("BluetoothService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
---
+++ src/mk/t/android/services/LocationService.java
@@ -0,0 +1,22 @@
+package mk.t.android.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tomislav.markovski
+ * Date: 2/14/12
+ * Time: 2:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LocationService extends IntentService {
+    public LocationService() {
+        super("LocationService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
