6b1e64cf22c8dfa2e72ba5c3e4e04cadc94dd38e
unknown <tomislav.markovski@Tomislav-Markov.seavus.biz>
2012-02-14 12:22:51 +0100

    changed package to mk.t.android

---
+++ src/mk/t/android/WifiService.java
@@ -0,0 +1,30 @@
+package mk.t.android;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Tome
+ * Date: 13.2.12
+ * Time: 22:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class WifiService extends IntentService {
+    public WifiService() {
+        super("WifiService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        WifiManager wifiManager = ((WifiManager) this.getSystemService(Context.WIFI_SERVICE));
+        wifiManager.setWifiEnabled(!isWifiEnabled(this));
+    }
+
+    public static boolean isWifiEnabled(Context context) {
+        WifiManager wifiManager = ((WifiManager) context.getSystemService(Context.WIFI_SERVICE));
+        return wifiManager.isWifiEnabled();
+    }
+}
